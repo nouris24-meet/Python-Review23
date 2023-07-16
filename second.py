a273c42dfd21d629f23d7fe7664da21aa475095a
nouris24-meet <nour.stety@gmail.com>
2023-07-16 15:34:18 +0300

    Create second.py
    I want to do the bonus later on

---
+++ second.py
@@ -0,0 +1,22 @@
+def create_youtube_video(title, description):
+	new_video={"title":title, "description":description, "likes":0, "dislikes":0, "comments": {"username": ""}}
+	return new_video
+def like(new_video):
+	if "likes" in new_video:
+		new_video["likes"]+=1
+	return new_video
+def dislike(new_video):
+	if "dislikes" in new_video:
+		new_video["dislikes"]+=1
+	return new_video
+		
+def add_comment(youtubevideo, username, comment_text):
+	youtubevideo["comments"][username]= comment_text
+	retun youtubevideo 
+
+example= create_youtube_video("cats","we love cats")
+like(example)
+dislike(example) 
+add_comment(example, "nour","wow")
+for i in range(495):
+	like(example)
